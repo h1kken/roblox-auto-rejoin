483d40728aa4a78834899d584fedba7e07386ce5
h1kken <kirill.prokofiev3003@mail.ru>
2026-04-17 09:07:08 +0300

    fixes

--- src/roblox.py
+++ src/roblox.py
@@ -11,7 +11,7 @@ from src.http import HttpClient
 from src.utils import log
 
 
-async def get_user_info(client: HttpClient) -> int | None:
+async def get_user_info(client: HttpClient) -> tuple[int | None, str | None, str | None]:
     try:
         response = await client.get(
             "https://users.roblox.com/v1/users/authenticated",
@@ -26,23 +26,50 @@ async def get_user_info(client: HttpClient) -> int | None:
         return None, None, None
 
 
+async def get_user_by_username(
+    client: HttpClient,
+    username: str,
+) -> tuple[int | None, str | None, str | None]:
+    try:
+        response = await client.post(
+            "https://users.roblox.com/v1/usernames/users",
+            json={
+                "usernames": [username],
+                "excludeBannedUsers": False,
+            },
+        )
+        users = response.json().get("data", [])
+        if not users:
+            return None, None, None
+
+        user = users[0]
+        user_id = user["id"]
+        name = user["name"]
+        display_name = user["displayName"]
+        return user_id, name, display_name
+    except Exception as e:
+        log(f"Failed to get User By Username ({type(e).__name__}): {e!r}", ANSI.RED)
+        return None, None, None
+
+
 async def get_place_id_user_in(
     client: HttpClient,
     *,
     user_id: int | None = None,
 ) -> int | None:
     if user_id is None:
-        user_id = await get_user_info(client)
+        user_id, _, _ = await get_user_info(client)
 
     if user_id is None:
         log("Failed to get Place ID: user_id is None", ANSI.RED)
         return None
 
-    data = { "userIds": [user_id] }
     try:
         response = await client.post(
             "https://presence.roblox.com/v1/presence/users",
-            json=data,
+            json={
+                "userIds": [user_id]
+            },
         )
         place_id = response.json()["userPresences"][0]["placeId"]
         return place_id
@@ -102,12 +129,6 @@ async def get_auth_ticket(
 
 async def get_job_id(client: HttpClient) -> str | None:
     try:
-        params = {
-            "sortOrder": int(FIRSTLY_SMALL_SERVERS),
-            "excludeFullGames": str(EXCLUDE_FULL_GAMES).lower(),
-            "limit": LIMIT_SERVERS_PER_PAGE,
-        }
-        
         server_types = [0]
         if JOIN_TO_FRIENDS == JoinToFriends.IF_AVAILABLE:
             server_types = [1, 0]
@@ -122,7 +143,11 @@ async def get_job_id(client: HttpClient) -> str | None:
 
             response = (await client.get(
                 f"https://games.roblox.com/v1/games/{PLACE_ID}/servers/{server_type}",
-                params=params,
+                params={
+                    "sortOrder": int(FIRSTLY_SMALL_SERVERS),
+                    "excludeFullGames": str(EXCLUDE_FULL_GAMES).lower(),
+                    "limit": LIMIT_SERVERS_PER_PAGE,
+                },
             )).json()
             
             servers = response.get("data", [])
